d496fb773c6673188e812e2f5be5c8022705d027
Gleb Lagutko <gleblagutko@gmail.com>
2020-03-21 15:10:39 +0300

    Initial commit

---
+++ src/sample/ControllerFirstTask.java
@@ -0,0 +1,95 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class ControllerFirstTask {
+    final static Pattern EMAIL = Pattern.compile("[a-z]([._]?[a-z]+)*[@][a-z][a-z]*[.]((org)|(net)|(ru)|(com)|(by))", Pattern.CASE_INSENSITIVE);
+
+    final static Pattern N = Pattern.compile("([0-9]*)([1-9])([0-9]*)");
+    final static Pattern Z = Pattern.compile("[-+]?([0-9]*)([0-9])([0-9]*)");
+    final static Pattern R = Pattern.compile("[-+]?(([0-9]*[.]?[0-9]+)|([0-9]+[.]?[0-9]*))([eE][-+]?[0-9]+)?");
+    final static Pattern DATE = Pattern.compile("([0-2][0-9]|(3)[0-1])((.)|(\\/)|(-))(((0)[0-9])|((1)[0-2]))((.)|(\\/)|(-))(\\d{4})");
+    final static Pattern TIME = Pattern.compile("(([0-1][0-9])|([2][0-3])):([0-5][0-9])");
+    final static String[] ELEMENTS = {"Natural", "Date", "E-Mail", "Time", "Real", "Integer"};
+
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private ComboBox<String> userComboBox;
+
+    @FXML
+    private Circle userCircle;
+
+
+    public void initialize() {
+
+        userComboBox.setItems(FXCollections.observableArrayList(ELEMENTS));
+        userComboBox.setValue("Integer");
+
+        usernameTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            Matcher m;
+            switch (userComboBox.getValue()) {
+                case "Natural":
+                    m = N.matcher(newValue);
+                    if (m.matches())
+                        userCircle.setFill(Color.GREEN);
+                    else
+                        userCircle.setFill(Color.RED);
+                    break;
+
+                case "Date":
+                    m = DATE.matcher(newValue);
+
+                    if (m.matches())
+                        userCircle.setFill(Color.GREEN);
+                    else
+                        userCircle.setFill(Color.RED);
+                    break;
+
+                case "Integer":
+                    m = Z.matcher(newValue);
+                    if (m.matches())
+                        userCircle.setFill(Color.GREEN);
+                    else
+                        userCircle.setFill(Color.RED);
+                    break;
+                case "E-Mail":
+                    m = EMAIL.matcher(newValue);
+                    if (m.matches())
+                        userCircle.setFill(Color.GREEN);
+                    else
+                        userCircle.setFill(Color.RED);
+                    break;
+
+                case "Time":
+                    m = TIME.matcher(newValue);
+                    if (m.matches())
+                        userCircle.setFill(Color.GREEN);
+                    else
+                        userCircle.setFill(Color.RED);
+                    break;
+
+                case "Real":
+                    m = R.matcher(newValue);
+                    if (m.matches())
+                        userCircle.setFill(Color.GREEN);
+                    else
+                        userCircle.setFill(Color.RED);
+                    break;
+            }
+        });
+    }
+}
+
---
+++ src/sample/ControllerSecondTask.java
@@ -0,0 +1,34 @@
+package sample;
+
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ControllerSecondTask {
+
+    final static Pattern DATE = Pattern.compile("([0-2][0-9]|(3)[0-1])((.)|(\\/)|(-))(((0)[0-9])|((1)[0-2]))((.)|(\\/)|(-))(\\d{4})");
+    final static String START_STRING = " ";
+
+
+    public TextArea inputArea;
+    public TextArea outputArea;
+
+    public void initialize() {
+        inputArea.setText(START_STRING);
+
+        inputArea.textProperty().addListener((observable, oldValue, newValue) -> {
+            outputArea.setText("");
+            String text = inputArea.getText();
+            Matcher matcher = DATE.matcher(text);
+            while (matcher.find()) {
+                outputArea.appendText(matcher.group());
+                outputArea.appendText("\n");
+            }
+        });
+    }
+}
